a2d0c978b085d6535abc717a7deb18955f8481ff
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-23 15:56:47 +0000

    separated the header and footer content to their respective files

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function followdylson_register_scripts(){
 
     wp_enqueue_script('followdylson-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
 
-    wp_enqueue_script('followdylson-bootstrap', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true);
+    wp_enqueue_script('followdylson-main', get_template_directory_uri()."/assets/js/main.js", array(), '1.0', true);
 
 }
 
