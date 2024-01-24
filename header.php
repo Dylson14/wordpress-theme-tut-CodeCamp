c48f77538ed7808d145500c1aa05d56d330a8ba2
Dylson Oliveira <dylsonuni@gmail.com>
2024-01-24 10:49:59 +0000

    added the main link to the blog in the site title

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 <body>
 
     <header class="header text-center">
-        <a class="site-title pt-lg-4 mb-0" href="index.html">
+        <a class="site-title pt-lg-4 mb-0" href="<?php echo site_url('/blog') ?>">
             <?php echo get_bloginfo('name') ?>
         </a>
 
